74b876d43155f29dbf94ece54bf121fe15cbca86
joellebegin <joellebegin.6@gmail.com>
2019-06-27 00:22:44 -0400

    modifying shell script for supercomputer

--- bubble_fields.sh
+++ bubble_fields.sh
@@ -1,23 +1,15 @@
 #!/bin/bash
+#SBATCH --time=00:02:00
+#SBATCH --mem=35000
+#SBATCH --cpus-per-task=10
+
 python gen_bubbles.py
 rm bub.txt
-mkdir fields
-mkdir results_figures
-mkdir results_data
 
-for f in *.txt; do
-    mv ${f} fields
-    done
+echo 'bubbles.txt' | mpiexec -n 4 python triangleCorrelation.py
+rm bubbles.txt
 
-for f in fields/*.txt; do
-    echo "### doing $f ###"
-    echo ${f} | mpiexec -n 5 python triangleCorrelation.py
-    done
+mv bubbles.txt.csv results_data
 
-for f in fields/*.csv; do
-    mv ${f} results_data
-    done
+mv bubbles.txt.png results_figures
 
-for f in fields/*.png; do
-    mv ${f} results_figures
-    done
